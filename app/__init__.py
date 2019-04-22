bd4ab6a38769ab7e9312ac63d854c8147ab20752
Antavio <njuguna13@gmail.com>
2019-04-22 09:49:36 +0300

    Create a User models with the table columns.Also initialize our registration form into our auth init file.

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,20 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
+from flask_login import LoginManager
 
 #Initialize Flask Extensions
 bootstrap = Bootstrap()
+
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 
 def create_app(config_name):
     app = Flask(__name__)
 
     #Initialize flask extensions
     bootstrap.init_app(app)
+    login_manager.init_app(app)
 
     # Register BluePrint
     from .main import main as main_blueprint
