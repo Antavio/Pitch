789b4418657c9959890fe4b9848314c71f508af3
Antavio <njuguna13@gmail.com>
2019-04-22 13:07:21 +0300

    Create a test_pitch unittest to test creation of pitches.

---
+++ tests/test_pitches.py
@@ -0,0 +1,20 @@
+from app.models import User,Pitch
+from app import db
+import unittest
+
+class PitchTest(unittest.TestCase):
+    def setUp(self):
+        self.new_user = User(username = 'Anto', password = 'pass', email = 'testemail@demo.com')
+        self.new_pitch = Pitch(id = 123, pitch_content = 'Pitch content',pitch_category = 'pickup',user=self.new_user)
+
+    def tearDown(self):
+        User.query.delete()
+        Pitch.query.delete()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_pitch,Pitch))
+
+    def test_check_instance_variables(self):
+        self.assertEquals(self.new_pitch.pitch_content,"Pitch content")
+        self.assertEquals(self.new_pitch.pitch_category,'pickup')
+        self.assertEquals(self.new_pitch.user,self.new_user)
