811a5680180712464f00d50b8bdfa6a0b578014c
Antavio <njuguna13@gmail.com>
2019-04-22 19:53:49 +0300

    Create a Pitch form in the main forms.py file. It displays a textArea & a dropdown for different pitch categories.

--- app/main/forms.py
+++ app/main/forms.py
@@ -5,3 +5,8 @@ from wtforms.validators import Required
 class UpdateProfile(FlaskForm):
     bio = TextAreaField('Tell us about you.',validators = [Required()])
     submit = SubmitField('Submit')
+
+class PitchForm(FlaskForm):
+    pitch = TextAreaField('Write Your Pitch',validators=[Required()])
+    pitch_category = SelectField('Pitch Category',choices=[('Technology-Pitch','Technology Pitch'),('Business-Pitch','Business Pitch'),('Interview-Pitch','Interview Pitch'),('Pickup-Line','Pickup-Line'),('Promotion-Pitch','Promotion')],validators=[Required()])
+    submit = SubmitField('Submit Pitch')
