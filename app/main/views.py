e760cbae757ac8ce0ef8f0bdd24ea1f069bfa968
Antavio <njuguna13@gmail.com>
2019-04-22 15:35:32 +0300

    Create a Profile html page with profile picture, avatar image and Bio information.

--- app/main/views.py
+++ app/main/views.py
@@ -1,4 +1,4 @@
-from flask import render_template,redirect,url_for
+from flask import render_template,redirect,url_for,request,abort
 from . import main
 
 @main.route('/')
@@ -8,3 +8,11 @@ def index():
     '''
     return render_template('index.html')
 
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
